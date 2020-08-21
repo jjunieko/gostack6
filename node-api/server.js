74c22a84cf5fd5b3229e7bf5f9f88bfefdd0c707
Joao Amancio Ribeiro Junior <joao.junior@cidadania.gov.br>
2020-08-21 17:21:27 -0300

    docker

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+
 
 const app = express();
 
 
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{useNewUrlParser: 
+    true});
+
+
+    requireDir('./src/models');
+
+
+    const Product = mongoose.model('Product');
+
+    
 app.get('/', (req, res) => {
+    Product.create({
+        title: "React Native",
+        description: "Build native apps with React",
+        url: "http://github.com/facebook/react-native"
+    })
     res.send("Hello Junior");
 });
 
